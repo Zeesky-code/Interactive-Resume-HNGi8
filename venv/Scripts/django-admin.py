c5ba276823d783ba93e074cb36f8219654966e0b
Zainab Lawal <zainablaw1012@gmail.com>
2021-08-17 17:54:49 +0300

    Installed requirements and created static files

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\zaina\onedrive\documents\github\interactive-resume-hngi8\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
